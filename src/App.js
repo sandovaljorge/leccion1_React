68cdcd42a4720427bce4a8475d74c5522c73f8ab
Jorge Sandoval <sandovalatin@gmail.com>
2023-03-04 16:14:09 -0600

    Commit inicial.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/estilos.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    
+    //constantes y funciones
+    const nombre = 'Jorge Armando Sandoval';
+    const saludo = 'Curso de React - Intecap';
+
+    return (
+        //render - lo que se muestra
+        //Fragment
+        <>  
+            <h1 className='titulo'>{saludo} <br /> {nombre}</h1>        
+        </>
+    )
 }
 
-export default App;
+export default App
